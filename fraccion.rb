b888158e79f05f5ba9ee254fb23a7c94f7cf3377
Mauricio José Orta Rodríguez <alu0100812930@ull.edu.es>
2015-10-24 17:24:26 +0000

    Creación de clase Fraccion

---
+++ fraccion.rb
@@ -0,0 +1,51 @@
+
+class Fraccion
+    attr_accessor :n  
+    attr_accessor :d
+    def initialize(n, d)
+        mcm = mcm(n,d)
+        @n = n/mcm
+        @d = d/mcm
+        
+    end
+    def mcm(a,b)
+            while (b!=0)
+            r=a%b
+            a=b
+            b=r
+            end
+            a
+    end
+    
+    def to_s
+        "#{@n}/#{@d}"
+    end
+    
+    def + (other)
+        if (@d==other.d)
+        Fraccion.new(@n + other.n, @d)
+    else
+        Fraccion.new(@n*other.d+other.n*@d, @d*other.d)
+        end
+    end
+    
+    def - (other)
+        if (@d==other.d)
+        Fraccion.new(@n-other.n, @d)
+    else
+        Fraccion.new(@n*other.d-other.n*@d, @d*other.d)
+        end
+    end
+    
+    def * (other)
+        Fraccion.new(@n*other.n, @d*other.d)
+    end
+    
+    def / (other)
+        Fraccion.new(@n*other.d, @d*other.n)
+    end
+    
+       
+end
+
+
